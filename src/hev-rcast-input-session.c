1d7b084f4c4172a059ac64292b863d08cb91ca25
Heiher <r@hev.cc>
2018-02-18 21:17:57 +0800

    HevRcastInputSession: Init.

---
+++ src/hev-rcast-input-session.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-input-session.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast input session
+ ============================================================================
+ */
+
+
+#include "hev-rcast-input-session.h"
---
+++ src/hev-rcast-input-session.h
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-input-session.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast input session
+ ============================================================================
+ */
+
+#ifndef __HEV_RCAST_INPUT_SESSION_H__
+#define __HEV_RCAST_INPUT_SESSION_H__
+
+#endif /* __HEV_RCAST_INPUT_SESSION_H__ */
+
