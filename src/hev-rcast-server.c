7e414936d8f3a49727b2494208080ca85b1a6c88
Heiher <r@hev.cc>
2018-02-18 20:38:31 +0800

    HevRcastServer: Init.

---
+++ src/hev-rcast-server.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-server.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast server
+ ============================================================================
+ */
+
+
+#include "hev-rcast-server.h"
---
+++ src/hev-rcast-server.h
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-server.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast server
+ ============================================================================
+ */
+
+#ifndef __HEV_RCAST_SERVER_H__
+#define __HEV_RCAST_SERVER_H__
+
+#endif /* __HEV_RCAST_SERVER_H__ */
+
