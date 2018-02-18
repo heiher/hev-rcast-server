a8c69a2ed318fe1f483041ae06cb4701339c6f9a
Heiher <r@hev.cc>
2018-02-18 21:19:09 +0800

    HevRcastOutputSession: Init.

---
+++ src/hev-rcast-output-session.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-output-session.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast output session
+ ============================================================================
+ */
+
+
+#include "hev-rcast-output-session.h"
---
+++ src/hev-rcast-output-session.h
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-output-session.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast output session
+ ============================================================================
+ */
+
+#ifndef __HEV_RCAST_OUTPUT_SESSION_H__
+#define __HEV_RCAST_OUTPUT_SESSION_H__
+
+#endif /* __HEV_RCAST_OUTPUT_SESSION_H__ */
+
