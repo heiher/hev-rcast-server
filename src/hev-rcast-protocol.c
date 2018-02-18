e8d9d13a90834172603b79b8d43296c43e3f1a72
Heiher <r@hev.cc>
2018-02-18 20:31:49 +0800

    HevRcastProtocol: Init.

---
+++ src/hev-rcast-protocol.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-protocol.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast protocol
+ ============================================================================
+ */
+
+#include "hev-rcast-protocol.h"
+
---
+++ src/hev-rcast-protocol.h
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-protocol.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast protocol
+ ============================================================================
+ */
+
+#ifndef __HEV_RCAST_PROTOCOL_H__
+#define __HEV_RCAST_PROTOCOL_H__
+
+#endif /* __HEV_RCAST_PROTOCOL_H__ */
+
