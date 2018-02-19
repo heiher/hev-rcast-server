d1628933405b9bd04b72294082c0c7a3005201c5
Heiher <r@hev.cc>
2018-02-19 14:06:00 +0800

    HevRcastProtocol: Define messages.

--- src/hev-rcast-protocol.h
+++ src/hev-rcast-protocol.h
@@ -10,5 +10,53 @@
 #ifndef __HEV_RCAST_PROTOCOL_H__
 #define __HEV_RCAST_PROTOCOL_H__
 
+typedef struct _HevRcastMessage HevRcastMessage;
+typedef struct _HevRcastMessageLogin HevRcastMessageLogin;
+typedef struct _HevRcastMessageFrame HevRcastMessageFrame;
+
+typedef enum _HevRcastMessageType HevRcastMessageType;
+typedef enum _HevRcastMessageLoginDirection HevRcastMessageLoginDirection;
+typedef enum _HevRcastMessageFrameType HevRcastMessageFrameType;
+
+enum _HevRcastMessageType
+{
+	HEV_RCAST_MESSAGE_LOGIN = 0,
+	HEV_RCAST_MESSAGE_FRAME,
+};
+
+enum _HevRcastMessageLoginDirection
+{
+	HEV_RCAST_MESSAGE_LOGIN_INPUT = 0,
+	HEV_RCAST_MESSAGE_LOGIN_OUTPUT,
+};
+
+enum _HevRcastMessageFrameType
+{
+	HEV_RCAST_MESSAGE_CFG_FRAME = 0,
+	HEV_RCAST_MESSAGE_KEY_FRAME,
+	HEV_RCAST_MESSAGE_REF_FRAME,
+};
+
+struct _HevRcastMessageLogin
+{
+	unsigned char direction;
+} __attribute__((packed));
+
+struct _HevRcastMessageFrame
+{
+	unsigned char type;
+	unsigned int length;
+} __attribute__((packed));
+
+struct _HevRcastMessage
+{
+	unsigned char type;
+
+	union {
+		HevRcastMessageLogin login;
+		HevRcastMessageFrame frame;
+	};
+} __attribute__((packed));
+
 #endif /* __HEV_RCAST_PROTOCOL_H__ */
 
