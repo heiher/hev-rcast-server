f507075e911bda5562d6bb9d281ad7114e9eed95
Heiher <r@hev.cc>
2018-02-19 11:56:35 +0800

    HevRcastTempSession: Implement new, ref/unref and run.

--- src/hev-rcast-temp-session.c
+++ src/hev-rcast-temp-session.c
@@ -7,5 +7,81 @@
  ============================================================================
  */
 
+#include <stdio.h>
 
 #include "hev-rcast-temp-session.h"
+
+#include <hev-task.h>
+#include <hev-memory-allocator.h>
+
+struct _HevRcastTempSession
+{
+	HevRcastBaseSession base;
+
+	int fd;
+	int ref_count;
+
+	HevRcastBaseSessionCloseNotify notify;
+	void *notify_data;
+};
+
+static void hev_rcast_task_entry (void *data);
+
+HevRcastTempSession *
+hev_rcast_temp_session_new (int fd,
+			HevRcastBaseSessionCloseNotify notify, void *data)
+{
+	HevRcastTempSession *self;
+
+	self = hev_malloc0 (sizeof (HevRcastTempSession));
+	if (!self) {
+		fprintf (stderr, "Alloc HevRcastTempSession failed!\n");
+		return NULL;
+	}
+
+	self->base.task = hev_task_new (-1);
+	if (!self->base.task) {
+		fprintf (stderr, "Create temp session task failed!\n");
+		hev_free (self);
+		return NULL;
+	}
+
+	self->base.hp = HEV_RCAST_BASE_SESSION_HP;
+
+	self->fd = fd;
+	self->ref_count = 1;
+	self->notify = notify;
+	self->notify_data = data;
+
+	return self;
+}
+
+HevRcastTempSession *
+hev_rcast_temp_session_ref (HevRcastTempSession *self)
+{
+	self->ref_count ++;
+
+	return self;
+}
+
+void
+hev_rcast_temp_session_unref (HevRcastTempSession *self)
+{
+	self->ref_count --;
+	if (self->ref_count)
+		return;
+
+	hev_free (self);
+}
+
+void
+hev_rcast_temp_session_run (HevRcastTempSession *self)
+{
+	hev_task_run (self->base.task, hev_rcast_task_entry, self);
+}
+
+static void
+hev_rcast_task_entry (void *data)
+{
+}
+
--- src/hev-rcast-temp-session.h
+++ src/hev-rcast-temp-session.h
@@ -10,5 +10,17 @@
 #ifndef __HEV_RCAST_TEMP_SESSION_H__
 #define __HEV_RCAST_TEMP_SESSION_H__
 
+#include "hev-rcast-base-session.h"
+
+typedef struct _HevRcastTempSession HevRcastTempSession;
+
+HevRcastTempSession * hev_rcast_temp_session_new (int fd,
+			HevRcastBaseSessionCloseNotify notify, void *data);
+
+HevRcastTempSession * hev_rcast_temp_session_ref (HevRcastTempSession *self);
+void hev_rcast_temp_session_unref (HevRcastTempSession *self);
+
+void hev_rcast_temp_session_run (HevRcastTempSession *self);
+
 #endif /* __HEV_RCAST_TEMP_SESSION_H__ */
 
