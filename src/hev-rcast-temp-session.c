9a5fa7ddafd323cae924180f27aa8cb84c046ddb
Heiher <r@hev.cc>
2018-02-19 10:36:25 +0800

    HevRcastTempSession: Init.

---
+++ src/hev-rcast-temp-session.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-temp-session.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast temp session
+ ============================================================================
+ */
+
+
+#include "hev-rcast-temp-session.h"
---
+++ src/hev-rcast-temp-session.h
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : hev-rcast-temp-session.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Rcast temp session
+ ============================================================================
+ */
+
+#ifndef __HEV_RCAST_TEMP_SESSION_H__
+#define __HEV_RCAST_TEMP_SESSION_H__
+
+#endif /* __HEV_RCAST_TEMP_SESSION_H__ */
+
