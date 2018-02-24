198192654afc7e5a0ed44b315c39fe3fd4f20aee
Heiher <r@hev.cc>
2018-02-25 07:07:58 +0800

    HevRcastHttpSession: Implement http message handler.

--- src/hev-rcast-http-session.c
+++ src/hev-rcast-http-session.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
@@ -102,6 +103,16 @@ hev_rcast_http_session_get_buffer (HevRcastHttpSession *self)
 	return buffer;
 }
 
+static int
+task_io_yielder (HevTaskYieldType type, void *data)
+{
+	HevRcastHttpSession *self = data;
+
+	hev_task_yield (type);
+
+	return (self->base.hp > 0) ? 0 : -1;
+}
+
 static void
 hev_rcast_task_entry (void *data)
 {
@@ -109,15 +120,59 @@ hev_rcast_task_entry (void *data)
 	HevTask *task = hev_task_self ();
 	HevRcastBaseSessionNotifyAction action;
 	HevRcastBuffer *buffer;
+	char buf[1024], *http = NULL;
+	size_t recv_len = 0, data_len;
+	ssize_t len;
+	unsigned int type = 0;
+	static const char *http_response = "HTTP/1.1 200 OK\r\n"
+		"Content-Type: application/json; charset=utf-8\r\n"
+		"Content-Length: 20\r\n\r\n"
+		"{ \"result\": \"succ\" }";
+	static size_t http_response_length = 0;
+
+	if (!http_response_length)
+		http_response_length = strlen (http_response);
 
 	hev_task_add_fd (task, self->base.fd, EPOLLIN | EPOLLOUT);
 
+	for (;;) {
+		len = hev_task_io_socket_recv (self->base.fd, buf + recv_len,
+					1023 - recv_len, 0, task_io_yielder, self);
+		if (0 >= len)
+			goto notify;
+		recv_len += len;
+
+		buf[recv_len] = '\0';
+		if (recv_len >= (2 + 11)) { /* T / HTTP/1.1\r|\n */
+			if (buf[0] != 'T' || buf[1] != ' ')
+				goto notify;
+
+			if ((http = strstr (buf, " HTTP/1.")))
+				break;
+		}
+	}
+
+	if (!http)
+		goto notify;
+
+	len = hev_task_io_socket_send (self->base.fd, http_response, http_response_length,
+				MSG_WAITALL, task_io_yielder, self);
+	if (len != http_response_length)
+		goto notify;
+
+	data_len = http - (buf + 2);
+	if ((data_len >= (2 + 5)) && (0 == memcmp (buf + 2, "/conf", 5)))
+		type = 1;
+
 retry_alloc:
-	buffer = hev_rcast_buffer_new (0, 0);
+	buffer = hev_rcast_buffer_new (type, data_len);
 	if (!buffer) {
 		hev_task_sleep (100);
 		goto retry_alloc;
 	}
+
+	data = hev_rcast_buffer_get_data (buffer);
+	memcpy (data, buf + 2, data_len);
 
 	/* dispatch frames */
 	if (self->buffer)
@@ -127,6 +182,7 @@ retry_alloc:
 	action = HEV_RCAST_BASE_SESSION_NOTIFY_DISPATCH;
 	self->notify ((HevRcastBaseSession *) self, action, self->notify_data);
 
+notify:
 	action = HEV_RCAST_BASE_SESSION_NOTIFY_FREE;
 	self->notify ((HevRcastBaseSession *) self, action, self->notify_data);
 }
