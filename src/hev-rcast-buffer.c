4f790d855504b3d891e5e59aceda96015b5a8f95
Heiher <r@hev.cc>
2018-10-17 21:25:08 +0800

    HevRcastBuffer: Sync to new coding style.

--- src/hev-rcast-buffer.c
+++ src/hev-rcast-buffer.c
@@ -13,63 +13,62 @@
 
 struct _HevRcastBuffer
 {
-	int ref_count;
+    int ref_count;
 
-	unsigned int length;
-	unsigned char type;
+    unsigned int length;
+    unsigned char type;
 
-	unsigned char data[0];
+    unsigned char data[0];
 };
 
 HevRcastBuffer *
 hev_rcast_buffer_new (unsigned char type, unsigned int length)
 {
-	HevRcastBuffer *self;
+    HevRcastBuffer *self;
 
-	self = hev_malloc (sizeof (HevRcastBuffer) + length);
-	if (!self)
-		return NULL;
+    self = hev_malloc (sizeof (HevRcastBuffer) + length);
+    if (!self)
+        return NULL;
 
-	self->type = type;
-	self->length = length;
-	self->ref_count = 1;
+    self->type = type;
+    self->length = length;
+    self->ref_count = 1;
 
-	return self;
+    return self;
 }
 
 HevRcastBuffer *
 hev_rcast_buffer_ref (HevRcastBuffer *self)
 {
-	self->ref_count ++;
+    self->ref_count++;
 
-	return self;
+    return self;
 }
 
 void
 hev_rcast_buffer_unref (HevRcastBuffer *self)
 {
-	self->ref_count --;
-	if (self->ref_count)
-		return;
+    self->ref_count--;
+    if (self->ref_count)
+        return;
 
-	hev_free (self);
+    hev_free (self);
 }
 
 void *
 hev_rcast_buffer_get_data (HevRcastBuffer *self)
 {
-	return self->data;
+    return self->data;
 }
 
 unsigned int
 hev_rcast_buffer_get_data_length (HevRcastBuffer *self)
 {
-	return self->length;
+    return self->length;
 }
 
 unsigned char
 hev_rcast_buffer_get_type (HevRcastBuffer *self)
 {
-	return self->type;
+    return self->type;
 }
-
--- src/hev-rcast-buffer.h
+++ src/hev-rcast-buffer.h
@@ -12,13 +12,12 @@
 
 typedef struct _HevRcastBuffer HevRcastBuffer;
 
-HevRcastBuffer * hev_rcast_buffer_new (unsigned char type, unsigned int length);
-HevRcastBuffer * hev_rcast_buffer_ref (HevRcastBuffer *self);
+HevRcastBuffer *hev_rcast_buffer_new (unsigned char type, unsigned int length);
+HevRcastBuffer *hev_rcast_buffer_ref (HevRcastBuffer *self);
 void hev_rcast_buffer_unref (HevRcastBuffer *self);
 
-void * hev_rcast_buffer_get_data (HevRcastBuffer *self);
+void *hev_rcast_buffer_get_data (HevRcastBuffer *self);
 unsigned int hev_rcast_buffer_get_data_length (HevRcastBuffer *self);
 unsigned char hev_rcast_buffer_get_type (HevRcastBuffer *self);
 
 #endif /* __HEV_RCAST_BUFFER_H__ */
-
