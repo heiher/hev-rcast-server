50c14847c65a8e6bdaddab2c626e1c13e0d829c4
Heiher <r@hev.cc>
2018-10-17 21:24:17 +0800

    HevMain: Sync to new coding style.

--- src/hev-main.c
+++ src/hev-main.c
@@ -21,54 +21,53 @@ static HevRcastServer *server;
 static void
 show_help (const char *self_path)
 {
-	printf ("%s CONFIG_PATH\n", self_path);
-	printf ("Version: %u.%u.%u\n", MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION);
+    printf ("%s CONFIG_PATH\n", self_path);
+    printf ("Version: %u.%u.%u\n", MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION);
 }
 
 static void
 signal_handler (int signum)
 {
-	quit ();
+    quit ();
 }
 
 int
 main (int argc, char *argv[])
 {
-	if (2 != argc) {
-		show_help (argv[0]);
-		return -1;
-	}
+    if (2 != argc) {
+        show_help (argv[0]);
+        return -1;
+    }
 
-	if (0 < hev_config_init (argv[1])) {
-		return -1;
-	}
+    if (0 < hev_config_init (argv[1])) {
+        return -1;
+    }
 
-	if (-1 == hev_task_system_init ()) {
-		fprintf (stderr, "Initialize task system failed!\n");
-		return -1;
-	}
+    if (-1 == hev_task_system_init ()) {
+        fprintf (stderr, "Initialize task system failed!\n");
+        return -1;
+    }
 
-	server = hev_rcast_server_new ();
-	if (!server) {
-		return -1;
-	}
+    server = hev_rcast_server_new ();
+    if (!server) {
+        return -1;
+    }
 
-	signal (SIGPIPE, SIG_IGN);
-	signal (SIGINT, signal_handler);
+    signal (SIGPIPE, SIG_IGN);
+    signal (SIGINT, signal_handler);
 
-	hev_rcast_server_run (server);
-	hev_task_system_run ();
+    hev_rcast_server_run (server);
+    hev_task_system_run ();
 
-	hev_rcast_server_destroy (server);
-	hev_task_system_fini ();
-	hev_config_fini ();
+    hev_rcast_server_destroy (server);
+    hev_task_system_fini ();
+    hev_config_fini ();
 
-	return 0;
+    return 0;
 }
 
 void
 quit (void)
 {
-	hev_rcast_server_quit (server);
+    hev_rcast_server_quit (server);
 }
-
